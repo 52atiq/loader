4860eeb2b249f88c08914dbd00e03184cd88e181
unknown <atiqit52@gmail.com>
2023-01-08 23:23:18 +0600

    second commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-const button = document.querySelector("#menu-button");
-const menu = document.querySelector("#menu");
+// const button = document.querySelector("#menu-button");
+// const menu = document.querySelector("#menu");
 
-button.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
+// button.addEventListener("click", () => {
+//   menu.classList.toggle("hidden");
+// });
+
+// $(document).ready(function () {
+//   setTimeout(() => {
+//     $(".loader").remove();
+//   }, 5000); 
+// });
+
+$(window).on('load', function(){
+  $("#preloader").fadeOut(1000);
 });
+
+// makes sure the whole site is loaded
